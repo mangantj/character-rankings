3455f3475b51ae1db7cdb400f21712bb24f297eb
Tyler Mangan <mangantj@gmail.com>
2024-03-08 15:43:14 -0600

    color code parses

--- app/helpers/character_search_helper.rb
+++ app/helpers/character_search_helper.rb
@@ -1,2 +1,13 @@
 module CharacterSearchHelper
+  def parse_color(number)
+    color = case number
+            when number..19.99 then "#666"
+            when number..49.99 then "#1eff00"
+            when number..74.99 then "#0070ff"
+            when number..94.99 then "#a335ee"
+            else 
+              "#ff8000"
+            end
+    color
+  end
 end
